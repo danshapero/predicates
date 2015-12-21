693b09b6267d8fd545fd67a9fb487c59c80b023d
danshapero <shapero.daniel@gmail.com>
2015-12-21 13:46:22 -0800

    Added fast/nonrobust predicates to main header for testing failures

--- include/predicates.h
+++ include/predicates.h
@@ -13,4 +13,16 @@ double insphere(double * pa,
                 double * pd,
                 double * pe);
 
+
+/* Fast but non-robust geometric predicates */
+double orient2dfast(double * pa, double * pb, double * pc);
+double orient3dfast(double * pa, double * pb, double * pc, double * pd);
+
+double incirclefast(double * pa, double * pb, double * pc, double * pd);
+double inspherefast(double * pa,
+                    double * pb,
+                    double * pc,
+                    double * pd,
+                    double * pe);
+
 #endif
