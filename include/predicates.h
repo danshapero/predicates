9778e5f2fe8dcb764db96fbd480397719297d138
danshapero <shapero.daniel@gmail.com>
2016-01-06 18:10:16 -0800

    Rewrote orient2d unit test in C++ using generic perturbation routine

--- include/predicates.h
+++ include/predicates.h
@@ -50,41 +50,6 @@ double inspherefast(const double * pa,
                     const double * pd,
                     const double * pe);
 
-
-/**
- * Routines for stress-testing geometric predicates.
- * These are used in both unit tests and demo programs.
- */
-
-typedef double (*orient2d_t)(const double *, const double *, const double *);
-typedef double (*incircle_t)(const double *,
-                             const double *,
-                             const double *,
-                             const double *);
-
-/**
- * Parameters:
- *    orientation: which geometric predicate to test; this must be either the
- *      address of `orient2d` or `orient2dfast`
- *    points: an array of the points around which we will be perturbing the
- *      inputs; a length-3 array of double[2]
- *    index: which input point to perturb; either 0, 1, 2
- *    grid_size: number of pixels along each dimension
- * Returns:
- *   zs: a heap-allocated (ny, nx)-array in row-major order of the orientations
- *     of all the perturbed inputs relative to the non-perturbed inputs.
- *     User is reponsible for destruction of this array.
- */
-double * perturb_orient2d(const orient2d_t orientation,
-                          const double ** points,
-                          const size_t index,
-                          const size_t grid_size);
-
-double * perturb_incircle(const incircle_t incircle,
-                          const double ** points,
-                          const size_t index,
-                          const size_t grid_size);
-
 #ifdef __cplusplus
 }
 #endif
---
+++ test/orient2d.cpp
@@ -0,0 +1,59 @@
+
+#include <iostream>
+#include <cassert>
+
+#include <cfloat>
+#include <cmath>
+
+#include <predicates.hpp>
+
+
+
+int main()
+{
+  /**
+   * Test a really easy case
+   */
+  double x1[] = {0.0, 0.0};
+  double x2[] = {1.0, 1.0};
+  double x3[] = {0.0, 1.0};
+
+  assert(orient2d(x1, x2, x3) > 0);
+  assert(orient2d(x1, x3, x2) < 0);
+
+
+  /**
+   * Test a harder case -- 3 nearly colinear points
+   */
+  double q1[] = {0.5, 0.5};
+  double q2[] = {12.0, 12.0};
+  double q3[] = {24.0, 24.0};
+
+  // Make 1D predicates out of the orientation predicate that we actually wish
+  // to test by fixing the other two points to check against.
+  // The orientation predicate is, in exact arithmetic, invariant to cyclic
+  // permutations of the inputs, so we should test that our floating-point
+  // version also respects this property by testing permutations of the inputs.
+  auto predicate1 = [&](const double * q) { return orient2d(q,  q2, q3); };
+  auto predicate2 = [&](const double * q) { return orient2d(q3, q,  q2); };
+  auto predicate3 = [&](const double * q) { return orient2d(q2, q3, q); };
+
+  // This is the callback for our perturbation routine; at every point, check
+  // if it is above or below the diagonal; points above should have positive
+  // orientation, points below negative, and 0 for points on the diagonal.
+  auto correct =
+    [](const double p, const double * q, const size_t, const size_t)
+    {
+      if (q[1] > q[0]) assert(p > 0);
+      else if (q[1] < q[0]) assert(p < 0);
+      else assert(p == 0.0);
+    };
+
+  // Check all points within a 256 x 256 grid of (0.5, 0.5).
+  perturb2d(predicate1, q1, 256, 256, correct);
+  perturb2d(predicate2, q1, 256, 256, correct);
+  perturb2d(predicate3, q1, 256, 256, correct);
+
+
+  return 0;
+}
