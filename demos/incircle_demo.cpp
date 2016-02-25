cceae882dafdddb7b3903f8d516546c071f54cf9
danshapero <shapero.daniel@gmail.com>
2016-02-25 15:53:18 -0800

    Made the incircle demo more interesting

--- demos/incircle_demo.cpp
+++ demos/incircle_demo.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <cfloat>
 
+#include <vector>
 #include <iostream>
 
 #include <predicates.hpp>
@@ -9,27 +10,54 @@
 
 using namespace predicates;
 
+
+bool is_square(int k)
+{
+  int q = int(std::round(std::sqrt(1.0 * k)));
+  return q*q == k;
+}
+
+
+/**
+ * Return a vector of all the integers `k` such that `n^2 - k^2` is a square
+ */
+std::vector<int> sums_of_squares(int n)
+{
+  std::vector<int> s;
+
+  for (int k = 1; k < n; ++k)
+    if (is_square(n*n - k*k))
+      s.push_back(k);
+
+  return s;
+}
+
+
 int main()
 {
-  const double x1[] = {1, 1};
-  const double x2[] = {0, 2};
-  const double x3[] = {-1, 1};
-  double x4[] = {0, 0};
+  const int n = 325;
 
-  const double q[] = {0.0, 1.0};
-  std::cout << incirclefast(x1, x2, x3, q) << std::endl;
+  std::vector<int> s = sums_of_squares(n);
 
-  const double r[] = {0.0, -1.0};
-  std::cout << incirclefast(x1, x2, x3, r) << std::endl;
+  // Pick out an integer `k` such that `n^2 - k^2` is an integer, and such
+  // that `k` and `sqrt(n^2 - k^2)` are roughly equal.
+  const int k = s[s.size()/2];
+  double x[] = {(double)k, -std::sqrt(n*n - k*k)};
+
+  // Pick some other integer points on the circle of radius `n`.
+  double q1[] = {(double)s[0] - x[0], std::sqrt(n*n - s[0]*s[0]) - x[1]};
+  double q2[] = {(double)s[1] - x[0], std::sqrt(n*n - s[1]*s[1]) - x[1]};
+  double q3[] = {(double)s[2] - x[0], std::sqrt(n*n - s[2]*s[2]) - x[1]};
 
   const size_t nx = 256, ny = 256;
+  double q4[] = {0.0, 0.0};
   for (size_t i = 0; i < nx / 2; ++i) {
-    x4[0] = nextafter(x4[0], x4[0] - 1);
-    x4[1] = nextafter(x4[1], x4[1] - 1);
+    q4[0] = nextafter(q4[0], q4[0] - 1);
+    q4[1] = nextafter(q4[1], q4[1] - 1);
   }
 
-  auto predicate = [&](const double * x)
-    { return incirclefast(x1, x2, x3, x); };
+  auto predicate = [&](const double * q)
+    { return incircle(q1, q2, q3, q); };
 
   double A[nx * ny];
 
@@ -39,7 +67,7 @@ int main()
       A[nx * i + j] = p;
     };
 
-  perturb2d(predicate, x4, nx, ny, callback);
+  perturb2d(predicate, q4, nx, ny, callback);
   matrix_to_png("incircle.png", nx, ny, A);
 
   return 0;
