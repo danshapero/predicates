cd56c948d12df78bce3f691dd3e821e3dd1d19e2
Michael Jackson <mike.jackson@bluequartz.net>
2016-08-29 20:29:45 -0400

    Adding CMake Config files to installation. Moving installed include files to a 'Predicates' subdirectory. updates #1 Adding more robust CTest support for compilers where the 'm' math library is already included (MSVC) Removing old code. updates #1

---
+++ src/cmake/PredicatesConfig.cmake
@@ -0,0 +1,5 @@
+include("${CMAKE_CURRENT_LIST_DIR}/PredicatesLibTargets.cmake")
+
+set(Predicates_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../include")
+set(Predicates_LIB_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../lib;${CMAKE_CURRENT_LIST_DIR}/../../bin")
+
