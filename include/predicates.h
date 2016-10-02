42bbdf3ae47df44058cb39d2c86a942cae61e3b0
danshapero <shapero.daniel@gmail.com>
2016-10-02 18:28:52 +0000

    Indentation

--- include/predicates.h
+++ include/predicates.h
@@ -16,17 +16,23 @@ namespace predicates {
  * Robust, adaptive-precision geometric predicates
  */
 
-PREDICATES_EXPORT double orient2d(const double * pa, const double * pb, const double * pc);
-PREDICATES_EXPORT double orient3d(const double * pa,
+PREDICATES_EXPORT
+double orient2d(const double * pa, const double * pb, const double * pc);
+
+PREDICATES_EXPORT
+double orient3d(const double * pa,
                 const double * pb,
                 const double * pc,
                 const double * pd);
 
-PREDICATES_EXPORT double incircle(const double * pa,
+PREDICATES_EXPORT
+double incircle(const double * pa,
                 const double * pb,
                 const double * pc,
                 const double * pd);
-PREDICATES_EXPORT double insphere(const double * pa,
+
+PREDICATES_EXPORT
+double insphere(const double * pa,
                 const double * pb,
                 const double * pc,
                 const double * pd,
@@ -37,17 +43,23 @@ PREDICATES_EXPORT double insphere(const double * pa,
  * Nonrobust, fast geometric predicates.
  */
 
-PREDICATES_EXPORT double orient2dfast(const double * pa, const double * pb, const double * pc);
-PREDICATES_EXPORT double orient3dfast(const double * pa,
+PREDICATES_EXPORT
+double orient2dfast(const double * pa, const double * pb, const double * pc);
+
+PREDICATES_EXPORT
+double orient3dfast(const double * pa,
                     const double * pb,
                     const double * pc,
                     const double * pd);
 
-PREDICATES_EXPORT double incirclefast(const double * pa,
+PREDICATES_EXPORT
+double incirclefast(const double * pa,
                     const double * pb,
                     const double * pc,
                     const double * pd);
-PREDICATES_EXPORT double inspherefast(const double * pa,
+
+PREDICATES_EXPORT
+double inspherefast(const double * pa,
                     const double * pb,
                     const double * pc,
                     const double * pd,
